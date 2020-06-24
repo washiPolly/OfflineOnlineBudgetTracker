bfefc4614e6cde13288a59265add923a1fe3c17b
washiPolly <pollylsu@gmail.com>
2020-06-24 00:57:18 -0700

    updated mongodb uri on server

--- server.js
+++ server.js
@@ -15,9 +15,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget"
-
-mongoose.connect(MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
 
 // routes
 app.use(require("./routes/api.js"));
